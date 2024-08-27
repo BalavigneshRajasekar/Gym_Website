d796a9a751307f712bf8e7b9c9bcd141b5d82c65
BalavigneshRajasekar <you@example.com>
2024-08-27 20:35:15 +0530

    New project setup created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center">Gym website</h1>
+    </>
+  );
+}
+
+export default App;
